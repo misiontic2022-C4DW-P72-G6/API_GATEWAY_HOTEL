c7cafe8a57d95f0cd1dcfcb6b6be333dee93d1b7
DanielAndresD <dadavilal@correo.udistrital.edu.co>
2021-12-05 12:48:30 -0500

    correccion index

--- src/typeDefs/index.js
+++ src/typeDefs/index.js
@@ -1,7 +1,7 @@
 const authTypeDefs = require('./auth_type_defs');
 const reservasTypeDefs = require('./reservas_type_defs');
 const catalogoTypeDefs = require('./catalogo_type_defs');
-const HabitacionTypeDefs = require('./Habitacion_type_defs');
+const HabitacionTypeDefs = require('./habitacion_type_defs');
 
 
 const schemasArrays = [authTypeDefs,catalogoTypeDefs,HabitacionTypeDefs,reservasTypeDefs];
